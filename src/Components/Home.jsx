73371b6f1d121983da7b086902154dfb70256110
Nikhilmukheja19 <128170642+Nikhilmukheja19@users.noreply.github.com>
2025-06-06 16:13:41 +0530

    Minor Chnges

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,17 +7,17 @@ import Navbar from "./Navbar";
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <header className="fixed top-0 left-0 w-full shadow-md bg-white z-50">
+      <header className="fixed top-0 left-0 right-0 w-full shadow-md bg-white z-50">
         <Navbar />
       </header>
       <main className="flex flex-1 flex-col gap-4 pt-16">
         <section className="w-full bg-white shadow rounded-lg">
           <HeroSection />
         </section>
-        <section className="top-0 left-0 w-full bg-white shadow rounded-lg">
+        <section className="w-full bg-white shadow rounded-lg">
           <ContentArea />
         </section>
-        <section className="top-0 left-0 w-full bg-white shadow rounded-lg">
+        <section className="w-full bg-white shadow rounded-lg">
           <ContactUs />
         </section>
       </main>
